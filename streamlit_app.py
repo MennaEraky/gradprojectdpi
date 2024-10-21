4561cbb903d400d1d90682d462b85a6cef77861f
MennaEraky <mennahesham62@yahoo.com>
2024-10-21 12:11:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,58 +9,84 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-# Sidebar Navigation
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Visualizations", "Model"])
+st.title("Australian Vehicle Prices")
 
-# Main content based on the selected page
-if page == "Home":
-    st.title("Australian Vehicle Prices")
-    st.image("https://github.com/MennaEraky/gradprojectdpi/blob/main/porsche-911-sally-cars-1.jpg", use_column_width=True)
+# Custom CSS for layout
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"] {
+        background: #e4f0ff;
+    }
 
-    st.markdown(
-        """
-        ## Overview
-        A comprehensive dataset for exploring the car market in Australia.
+    /* Container for the image and the text */
+    .container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
 
-        ### About Dataset
-        **Description**:
-        This dataset contains the latest information on car prices in Australia for the year 2023. It covers various brands, models, types, and features of cars sold in the Australian market. It provides useful insights into the trends and factors influencing the car prices in Australia. The dataset includes information such as brand, year, model, car/suv, title, used/new, transmission, engine, drive type, fuel type, fuel consumption, kilometres, colour (exterior/interior), location, cylinders in engine, body type, doors, seats, and price. The dataset has over 16,000 records of car listings from various online platforms in Australia.
+    /* Image on the left */
+    .image {
+        flex: 1;
+        margin-right: 20px;
+    }
 
-        ### Key Features
-        - **Brand**: Name of the car manufacturer
-        - **Year**: Year of manufacture or release
-        - **Model**: Name or code of the car model
-        - **Car/Suv**: Type of the car (car or suv)
-        - **Title**: Title or description of the car
-        - **UsedOrNew**: Condition of the car (used or new)
-        - **Transmission**: Type of transmission (manual or automatic)
-        - **Engine**: Engine capacity or power (in litres or kilowatts)
-        - **DriveType**: Type of drive (front-wheel, rear-wheel, or all-wheel)
-        - **FuelType**: Type of fuel (petrol, diesel, hybrid, or electric)
-        - **FuelConsumption**: Fuel consumption rate (in litres per 100 km)
-        - **Kilometres**: Distance travelled by the car (in kilometres)
-        - **ColourExtInt**: Colour of the car (exterior and interior)
-        - **Location**: Location of the car (city and state)
-        - **CylindersinEngine**: Number of cylinders in the engine
-        - **BodyType**: Shape or style of the car body (sedan, hatchback, coupe, etc.)
-        - **Doors**: Number of doors in the car
-        - **Seats**: Number of seats in the car
-        - **Price**: Price of the car (in Australian dollars)
+    /* Text content on the right */
+    .text-content {
+        flex: 2;
+        margin-left: 20px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-        ### Potential Use Cases
-        - **Price prediction**: Predict the price of a car based on its features and location using machine learning models.
-        - **Market analysis**: Explore the market trends and demand for different types of cars in Australia using descriptive statistics and visualization techniques.
-        - **Feature analysis**: Identify the most important features that affect car prices and how they vary across different brands, models, and locations using correlation and regression analysis.
-        """,
-        unsafe_allow_html=True,
-    )
+# Displaying the content in a custom HTML container
+st.markdown(
+    """
+    <div class="container">
+        <div class="image">
+            <img src="https://github.com/MennaEraky/gradprojectdpi/blob/main/porsche-911-sally-cars-1.jpg?raw=true" style="width: 100%; height: auto;">
+        </div>
+        <div class="text-content">
+            <h2>Australian Vehicle Prices</h2>
+            <p>A comprehensive dataset for exploring the car market in Australia.</p>
+            <h3>About Dataset</h3>
+            <p>This dataset contains the latest information on car prices in Australia for the year 2023. It covers various brands, models, types, and features of cars sold in the Australian market. It provides useful insights into the trends and factors influencing the car prices in Australia. The dataset includes information such as brand, year, model, car/suv, title, used/new, transmission, engine, drive type, fuel type, fuel consumption, kilometres, colour (exterior/interior), location, cylinders in engine, body type, doors, seats, and price. The dataset has over 16,000 records of car listings from various online platforms in Australia.</p>
 
-elif page == "Visualizations":
-    st.title("Visualizations")
-    st.write("This page will contain visualizations based on the dataset.")
+            <h3>Key Features:</h3>
+            <ul>
+                <li><strong>Brand:</strong> Name of the car manufacturer</li>
+                <li><strong>Year:</strong> Year of manufacture or release</li>
+                <li><strong>Model:</strong> Name or code of the car model</li>
+                <li><strong>Car/Suv:</strong> Type of the car (car or suv)</li>
+                <li><strong>Title:</strong> Title or description of the car</li>
+                <li><strong>UsedOrNew:</strong> Condition of the car (used or new)</li>
+                <li><strong>Transmission:</strong> Type of transmission (manual or automatic)</li>
+                <li><strong>Engine:</strong> Engine capacity or power (in litres or kilowatts)</li>
+                <li><strong>DriveType:</strong> Type of drive (front-wheel, rear-wheel, or all-wheel)</li>
+                <li><strong>FuelType:</strong> Type of fuel (petrol, diesel, hybrid, or electric)</li>
+                <li><strong>FuelConsumption:</strong> Fuel consumption rate (in litres per 100 km)</li>
+                <li><strong>Kilometres:</strong> Distance travelled by the car (in kilometres)</li>
+                <li><strong>ColourExtInt:</strong> Colour of the car (exterior and interior)</li>
+                <li><strong>Location:</strong> Location of the car (city and state)</li>
+                <li><strong>CylindersinEngine:</strong> Number of cylinders in the engine</li>
+                <li><strong>BodyType:</strong> Shape or style of the car body (sedan, hatchback, coupe, etc.)</li>
+                <li><strong>Doors:</strong> Number of doors in the car</li>
+                <li><strong>Seats:</strong> Number of seats in the car</li>
+                <li><strong>Price:</strong> Price of the car (in Australian dollars)</li>
+            </ul>
 
-elif page == "Model":
-    st.title("Model")
-    st.write("This page will contain the model for predicting car prices.")
-
+            <h3>Potential Use Cases:</h3>
+            <ul>
+                <li><strong>Price prediction:</strong> Predict the price of a car based on its features and location using machine learning models.</li>
+                <li><strong>Market analysis:</strong> Explore the market trends and demand for different types of cars in Australia using descriptive statistics and visualization techniques.</li>
+                <li><strong>Feature analysis:</strong> Identify the most important features that affect the car prices and how they vary across different brands, models, and locations using correlation and regression analysis.</li>
+            </ul>
+        </div>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
