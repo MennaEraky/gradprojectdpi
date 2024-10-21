ec7b12c0c479cb46d3d50f4a06a69a645e1aac8a
MennaEraky <mennahesham62@yahoo.com>
2024-10-21 11:52:43 +0300

    Create Model.py

---
+++ my_streamlit_app/Model.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Model",
+    page_icon=":robot_face:",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+st.title("Model")
+st.write("This page will be used for model predictions and insights.")
