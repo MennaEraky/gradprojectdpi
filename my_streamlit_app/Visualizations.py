4e26268851defc252f26ce089dcdaa28d017296c
MennaEraky <mennahesham62@yahoo.com>
2024-10-21 11:52:23 +0300

    Create Visualizations.py

---
+++ my_streamlit_app/Visualizations.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Visualizations",
+    page_icon=":bar_chart:",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+st.title("Visualizations")
+st.write("This page will contain visualizations related to the data.")
